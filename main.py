73d5c264e39e4eec03384aa3274dbc11992a2005
plinygg <aidan.chang@outlook.com>
2025-02-07 09:45:16 -0600

    improved overall features

--- main.py
+++ main.py
@@ -5,20 +5,14 @@ def main():
     required = []
     
     ticker = 1
-    prefix = input("what is the time constraint for n?")
-    if str_checker(prefix):
-        print("Invalid. try again")
-        return
-    time_constraint = int(prefix)
-
     while True:
-        dyna_string = "What goes on line #{a}? \n1. one number, the length of an array \n2. one number \n3. an array of numbers \n4. a set of numbers (n, m)\n5. a string \n6. that's it\n".format(a=str(ticker))
+        dyna_string = "What goes on line #{a}? \n1. one number, the length of an array \n2. one number \n3. an array of numbers \n4. a set of numbers (n, m)\n5. a string \n6. an array of strings \n7. that's it\n".format(a=str(ticker))
         queue = input(dyna_string)
-        if str_checker(queue) or int(queue) > 6:
+        if str_checker(queue) or int(queue) > 7:
             print("invalid input. please try again")
             return
         queue = int(queue)
-        if queue == 6:
+        if queue == 7:
             break
         required.append(queue)
         ticker += 1
@@ -28,16 +22,59 @@ def main():
     for part in required:
         temp = []
         if part == 1:
+            time_constraint = input("Are there time constraints to this number?")
+            if str_checker(time_constraint):
+                print('invalid input. please try again')
+                return
+            time_constraint = int(time_constraint)
             rand = random.randrange(0, time_constraint)
             temp.append(rand)
             len_array = rand
         elif part == 2:
-            temp.append(random.randrange(0, 10000))
+            time_constraint = input("Are there time constraints to this number?")
+            if str_checker(time_constraint):
+                print('invalid input. please try again')
+                return
+            time_constraint = int(time_constraint)
+            temp.append(random.randrange(0, time_constraint))
         elif part == 3:
+            time_constraint = input("Are there constraints to the size of an individual number?")
+            if str_checker(time_constraint):
+                print('invalid input. please try again')
+                return
+            time_constraint = int(time_constraint)
             if len_array != -1:
                 for _ in range(len_array):
-                    temp.append(random.randrange(0, 100000)) 
-                    # for next time, see if it is possible to implement the ability to only have small amounts of numbers               
-    
-    
+                    temp.append(random.randrange(0, time_constraint))
+        elif part == 4:
+            number = input("how many numbers are in the line?")
+            if str_checker(number):
+                print('broooo invalid')
+                return
+            number = int(number)
+            time_constraint = input("Are there constraints to the size of an individual number?")
+            if str_checker(time_constraint):
+                print('invalid input. please try again')
+                return
+            time_constraint = int(time_constraint)
+            for _ in range(number):
+                temp.append(random.randrange(0, time_constraint))
+        elif part == 5:
+            time_constraint = input("how long do you want the string to be?")
+            if str_checker(number):
+                print('broooo invalid')
+                return
+            time_constraint = int(time_constraint)
+            allowed = input("how many letters are allowed? input all for all")
+            if allowed != "all" or str_checker(allowed):
+                print('invalid input')
+                return
+            allowed = int(allowed)
+            letters = []
+            for _ in range(len(allowed)):
+                letter = input("input an allowed letter")
+                if len(letter) > 1 or not str_checker(letter):
+                    print('invalid input')
+                    return
+                letters.append(letter)
 main()
